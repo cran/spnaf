5aa246f65d3fe071aeed61a0b896f60f872821c3
Youngbin Lee <youngbin@snu.ac.kr>
2024-07-28 09:20:02 +0000

    version 1.1.0

--- R/SpatialWeight.R
+++ R/SpatialWeight.R
@@ -10,62 +10,63 @@
 #' @importFrom spdep nbdists
 
 SpatialWeight <- function(df, shape, snap = NULL, method = "queen", k = NULL,
-                          d = NULL, idw=FALSE, row_standardized= FALSE) {
+                          d = NULL, idw = FALSE, row_standardized = FALSE) {
     cat("(1) Creating Spatial Weights... ")
 
     w <- NULL
     original_id <- as.character(shape$id)
 
-    if(method == "queen") {
-        if (!inherits(shape, "SpatialPolygons") && !any(st_geometry_type(shape) == "POLYGON")) {
-            centroids <- st_centroid(st_as_sf(shape))
-            shape <- create_thiessen_polygons(centroids)
+    if(method == "queen"){
+        if(inherits(shape, "SpatialPoints") || any(st_geometry_type(shape) == "POINT")){
+            nb <- spdep::knearneigh(st_coordinates(shape), k = 8) # 8-nearest neighbors for queen
+            nb <- spdep::knn2nb(nb, sym = TRUE)
+        }else{
+            nb <- unclass(spdep::poly2nb(shape, snap = snap, queen = TRUE)) # create nb data from input shape
         }
-        nb <- unclass(spdep::poly2nb(shape, snap = snap, queen = TRUE)) # create nb data from input shape
-    } else if (method == "KNN") {
-        coo <- st_centroid(shape)
-        nb <- unclass(spdep::knn2nb(spdep::knearneigh(coo, k = k))) # create nb data from input shape
-    } else if (method == "fixed_distance") {
-        if (!inherits(shape, "SpatialPolygons") && !any(st_geometry_type(shape) == "POLYGON")) {
-            centroids <- st_centroid(st_as_sf(shape))
-            shape <- create_thiessen_polygons(centroids)
+    }else if(method == "KNN"){
+        if(inherits(shape, "SpatialPoints") || any(st_geometry_type(shape) == "POINT")){
+            nb <- spdep::knn2nb(spdep::knearneigh(st_coordinates(shape), k = k)) # create nb data from input shape
+        }else{
+            coo <- st_centroid(shape)
+            nb <- unclass(spdep::knn2nb(spdep::knearneigh(coo, k = k))) # create nb data from input shape
         }
-        nb <- spdep::poly2nb(shape, snap = snap, queen = TRUE)
-        nb2 <- spdep::nblag(neighbours = nb, maxlag = 2)
-        nb <- unclass(nb2[[2]])
+    }else if(method == "fixed_distance"){
+        if(inherits(shape, "SpatialPoints") || any(st_geometry_type(shape) == "POINT")){
+            nb <- spdep::dnearneigh(st_coordinates(shape), d1 = 0, d2 = d) # create distance-based neighbors
+        }else{
+            nb <- spdep::poly2nb(shape, snap = snap, queen = TRUE)
+            nb2 <- spdep::nblag(neighbours = nb, maxlag = 2)
+            nb <- unclass(nb2[[2]])
+        }
     }
 
-    nb <- lapply(nb, function(data) {
+    nb <- lapply(nb, function(data){
         return(original_id[data]) # substitute adjacent index for original id
     })
     names(nb) <- original_id # reference index for original id
 
-    if (idw) {
-        if (!inherits(shape, "SpatialPolygons") && !any(st_geometry_type(shape) == "POLYGON")) {
-            centroids <- st_centroid(st_as_sf(shape))
-            shape <- create_thiessen_polygons(centroids)
-        }
+    if(idw){
         coordinates_matrix <- as.matrix(st_coordinates(st_centroid(st_as_sf(shape))))
-        dist_matrix <- nbdists(nb, coordinates_matrix, longlat = TRUE)
+        dist_matrix <- spdep::nbdists(nb, coordinates_matrix, longlat = TRUE)
         inv_dist_list <- lapply(dist_matrix, function(x) 1 / x)
         inv_dist_list <- lapply(inv_dist_list, function(x) ifelse(is.infinite(x), 0, x))
-        if (row_standardized) {
+        if(row_standardized){
             inv_dist_list <- lapply(inv_dist_list, function(x) x / sum(x))
         }
-    } else {
+    }else{
         weight_list <- lapply(nb, function(x) rep(1, length(x)))
-        if (row_standardized) {
+        if(row_standardized){
             weight_list <- lapply(weight_list, function(x) x / sum(x))
         }
     }
 
-    if (idw) {
+    if(idw){
         nb_frame <- data.frame(
             oid = rep(names(nb), sapply(nb, length)),
             did = unlist(nb),
             w = unlist(inv_dist_list)
         )
-    } else {
+    }else{
         nb_frame <- data.frame(
             oid = rep(names(nb), sapply(nb, length)),
             did = unlist(nb),
@@ -96,12 +97,12 @@ SpatialWeight <- function(df, shape, snap = NULL, method = "queen", k = NULL,
     result <- U %>%
         dplyr::left_join(result, by = c("oid", "did")) %>%
         dplyr::mutate(n = ifelse(is.na(.data$n), 0, .data$n),
-                      w = ifelse(is.na(.data$w), 0, 1))
+                      w = ifelse(is.na(w), 0, 1))
 
     cat("Done!\n")
     # Alarm the size of the union set
     cat(paste0("note: Total ", nrow(U), " network combinations are ready to be analyzed\n"))
-    cat(paste0("note: which is calculated by the input polygon data\n"))
+    cat(paste0("note: which is calculated by the input data\n"))
 
     return(result)
 }
